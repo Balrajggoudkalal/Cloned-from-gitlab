007c61798fa53d30dc5200f52dd8977c05942750
Raghu K <raghuk.vit@gmail.com>
2019-04-23 19:30:10 +0530

    --- Auto-Git Commit ---

--- ami-setup.sh
+++ ami-setup.sh
@@ -63,4 +63,6 @@ echo "*/10 * * * * sh -x /boot/idle.sh &>/tmp/idle.out" >/var/spool/cron/root
 chmod 600 /var/spool/cron/root
 STAT2=$?
 if [ $STAT1 -eq 0 -a $STAT2 -eq 0 ]; then 
-    STAT
+    STAT=$?
+else
+    STAT=
