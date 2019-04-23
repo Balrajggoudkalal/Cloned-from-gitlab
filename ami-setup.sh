4ec341acd67ed7b80269534b410b40e5a22d9b68
Raghu K <raghuk.vit@gmail.com>
2019-04-23 17:43:23 +0530

    --- Auto-Git Commit ---

--- ami-setup.sh
+++ ami-setup.sh
@@ -3,7 +3,8 @@
 ## Following code can help in setting up AMI in AWS for practice of DevOps Tools 
 
 ## Check ROOT USER 
-if [ $()]
+if [ $(id -u) -ne 0 ]; then 
+    Error "You should be a root user to perform this script"
 
 
 ## Disabling SELINUX
