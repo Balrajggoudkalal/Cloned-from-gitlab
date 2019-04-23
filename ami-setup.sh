100a1753d0aa332eb81f4157db3839c711d5f44a
Raghu K <raghuk.vit@gmail.com>
2019-04-23 19:07:43 +0530

    --- Auto-Git Commit ---

--- ami-setup.sh
+++ ami-setup.sh
@@ -38,5 +38,5 @@ PACK_LIST="wget zip unzip gzip vim net-tools https://dl.fedoraproject.org/pub/ep
 info "Installing Base Packages"
 for package in $PACK_LIST ; do 
     yum install $package -y &>/dev/null  
-    Stat $? "Installed $package"
+    Statt $? "Installed $package"
 done
