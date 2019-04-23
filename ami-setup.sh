5fd360b6913ce3e79ea7586b161bfaa890d75536
Raghu K <raghuk.vit@gmail.com>
2019-04-23 19:41:07 +0530

    --- Auto-Git Commit ---

--- ami-setup.sh
+++ ami-setup.sh
@@ -76,3 +76,4 @@ echo -e "LANG=en_US.utf-8\nLC_ALL=en_US.utf-8" >/etc/environment
 ROOT_PASS=$(head /dev/urandom | tr -dc A-Za-z | head -c 8 ; echo '')
 CENTOS_PASS=$(head /dev/urandom | tr -dc A-Za-z | head -c 8 ; echo '')
 sed -i -e '/^PasswordAuthentication/ c PasswordAuthentication yes' /etc/ssh/sshd_config
+Stat $? "Enable Password Login"
