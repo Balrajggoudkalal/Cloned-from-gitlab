07b49b3fbc244facc5801032319154b7ab336ad2
Raghu K <raghuk.vit@gmail.com>
2019-09-30 03:26:14 +0000

    Update ps1.sh

--- scipts/ps1.sh
+++ scipts/ps1.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-if [ `id -u` = 0 ]; then 
-  export PS1='\[\e[1;31m\][\u@\h \W]# \[\e[0m\]'
-else
-  export PS1='\[\e[1;31m\][\u@\h \W]$ \[\e[0m\]'
+AWS_HOSTNAME=$(aws ec2 describe-instances --region us-east-2 --instance-ids $(curl -s http://169.254.169.254/latest/meta-data/instance-id) --query   "Reservations[*].Instances[*].[Tags[*]]" | grep Name -B1  | head -1 | grep Value  |xargs | awk -F : '{print $2}' | sed -e 's/,//g' -e 's/ //g')
+
+if [ -z "$AWS_HOSTNAME" ]; then 
+    if [ `id -u` = 0 ]; then 
+      export PS1='\[\e[1;31m\][\u@\h \W]# \[\e[0m\]'
+    else
+      export PS1='\[\e[1;31m\][\u@\h \W]$ \[\e[0m\]'
+    fi
+
+else 
+    if [ `id -u` = 0 ]; then 
+      export PS1='\[\e[1;31m\][\u@$AWS_HOSTNAME \W]# \[\e[0m\]'
+    else
+      export PS1='\[\e[1;31m\][\u@$AWS_HOSTNAME \W]$ \[\e[0m\]'
+    fi
 fi
